6c50061d0b2fa380f4ab7d5d8ed8a45e92b287c7
Eric McCormick <edm00se@gmail.com>
2016-09-08 13:39:55 -0500

    test(test/app.js): app test updated to cover Bootstrap3 theme logic
    using a BS theme requires ExtLib

--- test/app.js
+++ test/app.js
@@ -36,8 +36,7 @@ describe('generator-xsp:app - app with bower', function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
       .withOptions({
         name: testProjName,
-        basetheme: 'Bootstrap3',
-        ddeplugins: 'com.ibm.xsp.extlib.library'
+        basetheme: 'Bootstrap3'
       })
       .withPrompts({
         installBower: true
