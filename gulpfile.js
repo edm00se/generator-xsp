7523336e22648253b4d64e34cc42faf9843b2056
Eric McCormick <edm00se@gmail.com>
2016-11-17 21:22:36 -0600

    chore(gulpfile): moved codecov push from gulpfile to travis config
    recommended codecov example uses travis script, gulp-codecov looks stale

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var eslintConfig = require('./.eslintrc.json');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var codecov = require('gulp-codecov');
 var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 
@@ -47,17 +46,9 @@ gulp.task('test', ['pre-test'], function (cb) {
     });
 });
 
-gulp.task('codecov', function () {
-  if (process.env.CI) {
-    return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-      .pipe(codecov());
-  }
-  return;
-});
-
 gulp.task('watch', function () {
   gulp.watch(['generators/**/*.js', 'test/**'], ['test']);
 });
 
 gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['static', 'test', 'codecov']);
+gulp.task('default', ['static', 'test']);
